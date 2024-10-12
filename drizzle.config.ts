dbea48b45764fb8a156893c715856207122f9431
w3barsi <darcybalaga@gmail.com>
2024-10-12 15:07:04 +0800

    refactor(db): turso now working

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,9 +4,11 @@ import { env } from "~/env";
 
 export default {
   schema: "./src/server/db/schema.ts",
+  driver: "turso",
   dialect: "sqlite",
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url: env.TURSO_DATABASE_URL,
+    authToken: env.TURSO_AUTH_TOKEN,
   },
   tablesFilter: ["borbon-id_*"],
 } satisfies Config;
