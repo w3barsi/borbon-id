80a197e8fa6e86821515ff5bff21c8effc2d5e3c
w3barsi <darcybalaga@gmail.com>
2025-01-27 14:49:15 +0800

    added filter my prod user

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,15 +2,22 @@ import { currentUser } from "@clerk/nextjs/server";
 import { Container } from "~/components/container";
 import { Button } from "~/components/ui/button";
 import { api, HydrateClient } from "~/trpc/server";
+import { redirect } from "next/navigation";
 import React from "react";
 
 import Header from "../header";
 import DataTable from "./table";
 
+const allowedUsers = [
+  "user_2nKMOf6hWjwXRhvspf1khf0NOSc",
+  "user_2nmQLK32pORSbgMDL2Sv5lIvLEv",
+];
+
 export default async function AdminPage() {
   const user = await currentUser();
   void api.student.getStudents.prefetch();
-  if (user?.id !== "user_2nKMOf6hWjwXRhvspf1khf0NOSc") {
+  if (!user) return redirect("/");
+  if (allowedUsers.includes(user?.id)) {
     return <Container>Cannot access page as {user?.id}</Container>;
   }
 
